efb2117dcc9ca8ce93b6d45aa768601130d84b4e
Agunggg <agungrio307@gmail.com>
2025-11-02 20:30:59 +0700

    Percobaan 3 (Finish)

--- P10/Triangle02.java
+++ P10/Triangle02.java
@@ -4,6 +4,24 @@ public class Triangle02 {
     public static void main(String[] args) {
         
         int n;
-        
+        int i = 0;
+
+        Scanner agung = new Scanner(System.in);
+
+        System.out.print("Masukkan angka n = ");
+        n = agung.nextInt();
+
+        while (i <= n) {
+            int j = 1;
+            while (j < i) {
+                System.out.print("*");
+
+                j++;
+            }
+
+            System.out.println("*");
+
+            i++;
+        }
     }
 }
