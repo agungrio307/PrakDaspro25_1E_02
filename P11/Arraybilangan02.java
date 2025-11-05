0a18784ddaccbb15d6935087929d0c58581990dc
agungrio307 <agungrio307@gamil.com>
2025-11-05 14:35:31 +0700

    P11_Percobaan 1_Pertanyaan 2

--- P11/Arraybilangan02.java
+++ P11/Arraybilangan02.java
@@ -3,12 +3,7 @@ package P11;
 public class Arraybilangan02 {
     public static void main(String[] args) {
         
-        int bil02[] = new int[4];
-
-        bil02[0] = 5;
-        bil02[1] = 13;
-        bil02[2] = -7;
-        bil02[3] = 17;
+        int bil02[] = {5,13,-7,17};
 
         System.out.println(bil02[0]);
         System.out.println(bil02[1]);
