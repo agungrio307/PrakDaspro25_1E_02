12d6af3ea9d518ba82f1d7bb5f44995eef2b42c6
agungrio307 <agungrio307@gamil.com>
2025-11-05 15:46:49 +0700

    P11_Percobaan 3_Pertanyaan 1

--- P11/ArrayRataNilai02.java
+++ P11/ArrayRataNilai02.java
@@ -8,6 +8,7 @@ public class ArrayRataNilai02 {
         int[] nilaiMhs02 = new int[10];
         double total02 = 0;
         double rata2_02;
+        int jumlahMhsLulus02 = 0;
 
         for (int i = 0; i < nilaiMhs02.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + " : ");
@@ -16,9 +17,14 @@ public class ArrayRataNilai02 {
 
         for (int i = 0; i < nilaiMhs02.length; i++) {
             total02 += nilaiMhs02[i];
+
+            if (nilaiMhs02[i] > 70) {
+                jumlahMhsLulus02++;
+            }
         }
 
         rata2_02 = total02/nilaiMhs02.length;
         System.out.println("Rata-rata nilai = " + rata2_02);
+        System.out.println("Banyak mahasiswa yang lulus = " + jumlahMhsLulus02);
     }
 }
