3e11314a24536170d259d83e9d7dbe947fb17385
agungrio307 <agungrio307@gamil.com>
2025-11-05 14:39:30 +0700

    P11_Percobaan 1_Pertanyaan 3

--- P11/Arraybilangan02.java
+++ P11/Arraybilangan02.java
@@ -5,9 +5,9 @@ public class Arraybilangan02 {
         
         int bil02[] = {5,13,-7,17};
 
-        System.out.println(bil02[0]);
-        System.out.println(bil02[1]);
-        System.out.println(bil02[2]);
-        System.out.println(bil02[3]);
+        for (int i = 0; i < 4; i++) {
+            System.out.println(bil02 [i]);
+        }
+    
     }
 }
