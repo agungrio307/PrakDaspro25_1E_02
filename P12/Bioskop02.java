7430f5b98def3624f62e60f965ea68848f376b75
agungrio307 <agungrio307@gamil.com>
2025-11-12 11:34:00 +0700

    P12_Percobaan 1

---
+++ P12/Bioskop02.java
@@ -0,0 +1,22 @@
+package P12;
+
+public class Bioskop02 {
+    public static void main(String[] args) {
+        
+        String[][] penonton02 = new String[4][2];
+        
+        penonton02[0][0] = "Amin";
+        penonton02[0][1] = "Bena";
+        penonton02[1][0] = "Candra";
+        penonton02[1][1] = "Dela";
+        penonton02[2][0] = "Eka";
+        penonton02[2][1] = "Farhan";
+        penonton02[3][0] = "Gisel";
+        penonton02[3][1] = "Null";
+
+        System.out.printf("%s \t %s \n", penonton02[0][0], penonton02 [0][1]);
+        System.out.printf("%s \t %s \n", penonton02[1][0], penonton02 [1][1]);
+        System.out.printf("%s \t %s \n", penonton02[2][0], penonton02 [2][1]);
+        System.out.printf("%s \t %s \n", penonton02[3][0], penonton02 [3][1]);
+    }
+}
