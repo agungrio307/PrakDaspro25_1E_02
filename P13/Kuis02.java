cd09c60ca404d5397faa13874f64ec4c60d660fd
agungrio307 <agungrio307@gamil.com>
2025-11-19 13:22:36 +0700

    P13_Modifikasi kuis

--- P13/Kuis02.java
+++ P13/Kuis02.java
@@ -20,14 +20,15 @@ public class Kuis02 {
         int sepeda02 = 1000000;
         double diskon02 = 0.05;
 
-        for (int i = 0; i < Penjualan02.length; i++) {
-            System.out.print(nama02[i]);
-            for (int j = 0; j < Penjualan02[i].length; j++) {
-                System.out.print("\n" + Bulan02[j]);
-                Penjualan02[i][j]=Penjualan02[i][j]*sepeda02;
-                System.out.print("  " + (Penjualan02[i][j]));
-            }
 
+        for (int i = 0; i < Penjualan02.length; i++) {
+            System.out.printf("%-20s %-20s %-20s %-20s %-20s %n",nama02[i],  Bulan02[0], Bulan02[1], Bulan02[2], Bulan02[3]);
+            System.out.print("                     ");
+            for (int j = 0; j < Penjualan02[i].length; j++) {
+                Penjualan02[i][j] = Penjualan02[i][j] * sepeda02;
+                System.out.print((Penjualan02[i][j]) + "              ");
+            }
+            System.out.println();
             System.out.println();
         }
 
