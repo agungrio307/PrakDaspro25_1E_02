7d64855497f854f033b480104b2ed2567eb79043
agungrio307 <agungrio307@gamil.com>
2025-11-19 10:33:59 +0700

    P12_Percobaan 3

---
+++ P12/Numbers02.java
@@ -0,0 +1,13 @@
+package P12;
+import java.util.Arrays;
+
+public class Numbers02 {
+    public static void main(String[] args) {
+        
+        int[][] myNumbers02 = new int[3][];
+        myNumbers02[0] = new int[5];
+        myNumbers02[1] = new int[3];
+        myNumbers02[2] = new int[1];
+
+    }
+}
