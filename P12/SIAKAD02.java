8d27059fd0d0ce1b80265849146532c808c1e4c9
agungrio307 <agungrio307@gamil.com>
2025-11-19 10:42:02 +0700

    P12_Percobaan 4_Pertanyaan1

--- P12/SIAKAD02.java
+++ P12/SIAKAD02.java
@@ -3,36 +3,44 @@ import java.util.Scanner;
 
 public class SIAKAD02 {
     public static void main(String[] args) {
-
-        Scanner agung = new Scanner(System.in);
         
-        int nilai02[][] = new int[4][3];
+        Scanner agung = new Scanner(System.in);
 
-        for (int i = 0; i < nilai02.length; i++) {
+        int jumlahSiswa02;
+        int jumlahMatkul02;
+
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        jumlahSiswa02 = agung.nextInt();
+        System.out.print("Masukkan jumlah mata kuliah : ");
+        jumlahMatkul02 = agung.nextInt();
+
+        int nilai02[][] = new int[jumlahSiswa02][jumlahMatkul02];
+
+        for (int i = 0; i < jumlahSiswa02; i++) {
             System.out.println("Input nilai mahasiswa ke-" + (i+1));
             
             double totalPerSiswa02 = 0;
-            for (int j = 0; j < nilai02[i].length; j++) {
+            for (int j = 0; j < jumlahMatkul02; j++) {
                 System.out.print("Mata kuliah " + (j+1) + " : ");
                 nilai02[i][j] = agung.nextInt();
 
                 totalPerSiswa02 += nilai02[i][j];
             }
 
-            System.out.println("Nilai rata-rata : "  + totalPerSiswa02/3);
+            System.out.println("Nilai rata-rata : "  + totalPerSiswa02/jumlahMatkul02);
         }
 
         System.out.println("\n================================");
         System.out.println("Rata-rata nilai setiap mata kuliah : ");
 
-        for (int j = 0; j < 3; j++) {
+        for (int j = 0; j < jumlahMatkul02; j++) {
 
             double totalPerMatkul02 = 0;
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < jumlahSiswa02; i++) {
                 totalPerMatkul02 += nilai02[i][j];    
             }
 
-            System.out.println("Mata kuliah " + (j+1) + " : " + totalPerMatkul02/4);
+            System.out.println("Mata kuliah " + (j+1) + " : " + totalPerMatkul02/jumlahSiswa02);
         }
     }
 }
