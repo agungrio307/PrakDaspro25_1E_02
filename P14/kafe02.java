05d2c959dcf5d8503d5707e0951b22d704774efe
Agunggg <agungrio307@gmail.com>
2025-11-28 16:59:48 +0700

    P14_Percobaan3

--- P14/kafe02.java
+++ P14/kafe02.java
@@ -1,9 +1,10 @@
 package P14;
+import java.util.Scanner;
 
 public class kafe02 {
     public static void Menu02(String namaPelanggan02, boolean isMember02, String kodePromo02) {
         System.out.println("Selamat datang, " + namaPelanggan02 + "!");
-
+        
         if (isMember02) {
             System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
         } 
@@ -26,9 +27,27 @@ public class kafe02 {
         System.out.println("5. Roti Bakar - Rp 10,000");   
         System.out.println("6. Mie Goreng - Rp 18,000");   
         System.out.println("=".repeat(20));   
-        System.out.println("Silahkan plih menu yang Anda inginkan.");   
+        System.out.println("Silahkan plih menu yang Anda inginkan.");
     }
-    public static void main(String[] args) {
-        Menu02("Andi", true, "DISKON50");
+
+    public static int hitungTotalHarga02(int pilihanMenu02, int banyakItem02) {
+        int[] hargaItems02 = {15000, 20000, 22000, 12000, 10000, 18000};
+        
+        int hargaTotal02 = hargaItems02[pilihanMenu02 - 1] * banyakItem02;
+        return hargaTotal02;
     }
-}
+        
+        public static void main(String[] args) {
+            Scanner agung = new Scanner(System.in);
+
+            Menu02("Andi", true, "DISKON50");
+            Menu02("Budi", true, "DISKON30");
+            System.out.print("\nMasukkan nomor menu yang ingin Anda pesan: ");
+            int pilihanMenu02 = agung.nextInt();  
+            System.out.print("Masukkan banyak item yang ingin Anda pesan: ");
+            int banyakItem02 = agung.nextInt();
+        
+            int totalHarga02 = hitungTotalHarga02(pilihanMenu02, banyakItem02);
+            System.out.println("Total harga yang harus dibayar: Rp " + totalHarga02);
+        }
+    }
