3e0e923187c1457c1a1f938b27f5990f435b2c04
Agunggg <agungrio307@gmail.com>
2025-11-30 19:20:17 +0700

    P14_Percobaan 4

--- P14/PengunjungCafe02.java
+++ P14/PengunjungCafe02.java
@@ -1,7 +1,17 @@
 package P14;
 
 public class PengunjungCafe02 {
+    public static void daftarPengunjung02(String...namaPengunjung02) {
+        System.out.println("Daftar Nama Pengunjung: ");
+
+        for (int i = 0; i < namaPengunjung02.length; i++) {
+            System.out.println("- " + namaPengunjung02[i]);
+        }
+    }
     public static void main(String[] args) {
+        daftarPengunjung02("Ali", "Budi", "Citra");
+        daftarPengunjung02("Andi");
+        daftarPengunjung02("Doni", "Eti", "Fahmi", "Galih");
         
     }
 }
